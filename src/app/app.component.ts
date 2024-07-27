26ee72f9509c1b5def41f1ad06f6da9ccdd09fca
Sonal Agrawal <sonalfeb7@gmail.com>
2024-07-27 18:15:37 +0530

    Created Workout List Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { WorkoutFormComponent } from "./workout-form/workout-form.component";
-
+import { WorkoutListComponent } from "./workout-list/workout-list.component";
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, WorkoutFormComponent],
+  imports: [RouterOutlet, WorkoutFormComponent, WorkoutListComponent],
   templateUrl: "./app.component.html"
 })
 export class AppComponent {
