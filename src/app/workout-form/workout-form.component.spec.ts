1d4b4a9c7970c83f64334402eccfb0e9e4c605fc
Sonal Agrawal <sonalfeb7@gmail.com>
2024-07-29 02:20:50 +0530

    Added a unit test

--- src/app/workout-form/workout-form.component.spec.ts
+++ src/app/workout-form/workout-form.component.spec.ts
@@ -1,5 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from "@angular/forms";
+import { MatSelectModule } from "@angular/material/select";
+import { MatCardModule } from "@angular/material/card";
+import {NgIf} from '@angular/common';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WorkoutFormComponent } from './workout-form.component';
 
 describe('WorkoutFormComponent', () => {
@@ -8,9 +17,21 @@ describe('WorkoutFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [WorkoutFormComponent]
-    })
-    .compileComponents();
+      declarations: [WorkoutFormComponent],
+      imports: [
+        ReactiveFormsModule,
+        FormGroup,
+        FormControl,
+        Validators,
+        NgIf,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCardModule,
+        MatSelectModule,
+        BrowserAnimationsModule
+      ],
+    }).compileComponents();
     
     fixture = TestBed.createComponent(WorkoutFormComponent);
     component = fixture.componentInstance;
@@ -19,5 +40,14 @@ describe('WorkoutFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should call onSubmit when form is submitted', () => {
+    spyOn(component, 'onSubmit');
+
+    // Simulate form submission
+    const form = fixture.nativeElement.querySelector('form');
+    form.dispatchEvent(new Event('submit'));
+
+    expect(component.onSubmit).toHaveBeenCalled();
   });
 });
